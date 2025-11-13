bbbde23db549a60a81314f9470e11c2682bccc7c
bbradford1 <42330009+bbradford1@users.noreply.github.com>
2025-11-13 10:00:35 -0600

    Update wrn_installer.sh

--- wrn_installer.sh
+++ wrn_installer.sh
@@ -5,7 +5,7 @@
 # Purpose: Download and prepare the WRN maintenance script
 
 TARGET=~/Downloads/wrn_v3.sh
-URL="https://raw.githubusercontent.com/bbradford1/WRN-V3/main/wrn_v6_A.sh"
+URL="https://raw.githubusercontent.com/bbradford1/WRN-V3/main/wrn_v6_c.sh"
 
 echo "------------------------------------------------"
 echo "WRN Installer Script"
